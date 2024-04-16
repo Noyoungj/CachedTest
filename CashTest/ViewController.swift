30ebbd29754ad16b7fcb0236142a3bb13928513d
noyoungj96 <yj.no14@konai.com>
2024-04-16 12:30:45 +0900

    Initial Commit

---
+++ CashTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CashTest
+//
+//  Created by 노영재(Youngjae No)_인턴 on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
